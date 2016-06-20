2657f2f080dfcfe3dfdf146e277ec345d3c1a5b9
Kyle Wolfe <Kyle Wolfe>
2016-06-20 19:36:09 -0400

    TS25D-06 - Twin Stick Behavior and Selfie Stick

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject playerCharacter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerCharacter = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.LookAt (playerCharacter.transform);
+	}
+}
---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour {
+
+	public float panSpeed = 10f;
+
+	private GameObject playerCharacter;
+	private Vector3 armRotation;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerCharacter = GameObject.FindGameObjectWithTag ("Player");
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		armRotation.y += CrossPlatformInputManager.GetAxis ("RHoriz") * panSpeed;
+		armRotation.x += CrossPlatformInputManager.GetAxis ("RVert") * panSpeed;
+
+		transform.position = playerCharacter.transform.position;
+		transform.rotation = Quaternion.Euler (armRotation);
+	}
+}
